45c89ad2d8ba35e21cf7e096df3bfd7ccc28dd5e
Emanuele Rizzo <emanuele.rizzo@external.cerved.com>
2023-05-16 10:03:25 +0200

    import non usati

--- src/test/java/com/test/security/SecurityApplicationTests.java
+++ src/test/java/com/test/security/SecurityApplicationTests.java
@@ -3,7 +3,6 @@ package com.test.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
@@ -20,7 +19,6 @@ class SecurityApplicationTests {
 	void encode() {
 		String key = "123";
 		PasswordEncoder encoder = new Pbkdf2PasswordEncoder("test",0,10,100);
-//		PasswordEncoder encoder = new BCryptPasswordEncoder(-1);
 		System.out.println(encoder.encode(key));
 		System.out.println(encoder.encode(key));
 		System.out.println(encoder.encode(key));
